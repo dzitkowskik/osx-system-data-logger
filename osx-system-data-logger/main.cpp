8849554147f77c0c01c26312fe06a21c0d42092f
Karol Dzitkowski <k.dzitkowski@gmail.com>
2014-11-26 23:44:15 +0100

    Initial Commit

---
+++ osx-system-data-logger/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  osx-system-data-logger
+//
+//  Created by Karol Dzitkowski on 26.11.2014.
+//  Copyright (c) 2014 Karol Dzitkowski. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
